d8881cd2b4eed02b69a997e8b356f004dad22fe4
rooster man <roostermanred@gmail.com>
2025-06-30 16:47:17 -0500

    fix stack name

--- bin/upload-service.ts
+++ bin/upload-service.ts
@@ -3,4 +3,4 @@ import * as cdk from 'aws-cdk-lib'
 import { UploadStack } from '../lib/upload-service-stack'
 
 const app = new cdk.App()
-new UploadStack(app, 'UploadServiceStack')
+new UploadStack(app, 'UploadStack')
